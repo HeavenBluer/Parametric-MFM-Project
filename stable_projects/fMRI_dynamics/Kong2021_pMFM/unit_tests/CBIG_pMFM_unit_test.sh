d933eae1b6ffc94dd33a0c599c8c953e9f1e113f
HeavenBluer <kxl920327@gmail.com>
2021-09-16 18:32:28 +0800

    Code Commit 1
    Why:
    
    *
    
    This change addresses the need by:
    
    *

---
+++ stable_projects/fMRI_dynamics/Kong2021_pMFM/unit_tests/CBIG_pMFM_unit_test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# this function is the unit test
+# Written by Kong Xiaolu and CBIG under MIT license: https://github.com/ThomasYeoLab/CBIG/blob/master/LICENSE.md
+
+python CBIG_pMFM_unit_test_wrapper.py
